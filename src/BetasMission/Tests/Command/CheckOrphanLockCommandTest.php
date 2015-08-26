303ddeb16fc3cfc087f00b59400c90204d8db960
ndreux <nicolas.dreux@mobpartner.com>
2015-08-26 16:42:24 +0200

    - Add tests

--- src/BetasMission/Tests/Command/CheckOrphanLockCommandTest.php
+++ src/BetasMission/Tests/Command/CheckOrphanLockCommandTest.php
@@ -5,6 +5,7 @@ namespace BetasMission\Tests\Command;
 use BetasMission\Command\CheckOrphanLockCommand;
 use BetasMission\Helper\Context;
 use BetasMission\Helper\Locker;
+use BetasMission\Helper\Mailer;
 use DateTime;
 use PHPUnit_Framework_MockObject_MockObject;
 use PHPUnit_Framework_TestCase;
@@ -102,5 +103,43 @@ class CheckOrphanLockCommandTest extends PHPUnit_Framework_TestCase
     public function executeWithContextLockDataProvider()
     {
         return [[Context::CONTEXT_MOVE], [Context::CONTEXT_REMOVE], [Context::CONTEXT_DOWNLOAD_SUBTITLE]];
+    }
+
+    /**
+     * @return void
+     */
+    public function testSendAlert()
+    {
+        $lockFile = '/tmp/test.lock';
+        $datetime = new DateTime();
+
+        /** @var PHPUnit_Framework_MockObject_MockObject|Mailer $mailer */
+        $mailer = $this->getMockBuilder('BetasMission\Helper\Mailer')
+            ->getMock();
+
+        $mailer->expects($this->once())
+            ->method('send');
+
+        /** @var PHPUnit_Framework_MockObject_MockObject|CheckOrphanLockCommand $command */
+        $command = $this->getMockBuilder('BetasMission\Command\CheckOrphanLockCommand')
+            ->setMethods(['getMailer'])
+            ->getMock();
+
+        $command->expects($this->once())
+            ->method('getMailer')
+            ->willReturn($mailer);
+
+        $command->sendAlert($lockFile, $datetime);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMailer()
+    {
+        $command = new CheckOrphanLockCommand();
+        $mailer  = $command->getMailer();
+
+        $this->assertInstanceOf('BetasMission\Helper\Mailer', $mailer);
     }
 }
